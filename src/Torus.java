3dcf84361a5b612c4eeb32f5f04eaf9f9ad74bb9
ochughtai <ochughtai@live.com>
2017-09-25 12:54:54 -0500

    12:54 PM 9/25
    Made changes to getSuccessors()

--- src/Torus.java
+++ src/Torus.java
@@ -13,11 +13,67 @@ class State {
 
 	public State[] getSuccessors() {
 		
+		//Index of the zero in the board (initialized to cause out of bounds error)
+		int zeroPos = 10;
+		
+		//Array of States to be returned
+		State[] successors = new State[4];
+		
+		//Find the index of the zero in the board
+		for (int i = 0; i < this.board.length; i++) {
+			if (this.board[i] == 0) {
+				zeroPos = i;
+			}
+		}
+		//Declare States
+		State state1;
+		State state2;
+		State state3;
+		State state4;
+		
+		//If the zero is in the first column
+		if (zeroPos == 0 || zeroPos == 3 || zeroPos == 6) {
+			state1 = new State(this.swap(zeroPos, ((zeroPos + 1) % 9)));
+			state2 = new State(this.swap(zeroPos, ((zeroPos + 2) % 9)));
+			state3 = new State(this.swap(zeroPos, ((zeroPos + 3) % 9)));
+			state4 = new State(this.swap(zeroPos, ((zeroPos + 6) % 9)));
+		} 
+		
+		//If the zero is in the second column
+		else if (zeroPos == 1 || zeroPos == 4 || zeroPos == 7) {
+			state1 = new State(this.swap(zeroPos, ((zeroPos + 1) % 9)));
+			state2 = new State(this.swap(zeroPos, ((zeroPos + 3) % 9)));
+			state3 = new State(this.swap(zeroPos, ((zeroPos + 6) % 9)));
+			state4 = new State(this.swap(zeroPos, ((zeroPos + 8) % 9)));
+		}
+		
+		//If the zero is in the third column
+		else if(zeroPos == 2 || zeroPos == 5 || zeroPos == 8) {
+			state1 = new State(this.swap(zeroPos, ((zeroPos + 3) % 9)));
+			state2 = new State(this.swap(zeroPos, ((zeroPos + 6) % 9)));
+			state3 = new State(this.swap(zeroPos, ((zeroPos + 7) % 9)));
+			state4 = new State(this.swap(zeroPos, ((zeroPos + 8) % 9)));
+		}
+			
+		
 		// TO DO: get all four successors and return them in sorted order
 		
 		return successors;
 	}
 
+	//Method to swap two array elements are the input indices
+	public int[] swap(int index1, int index2) {
+		
+		//Copy board
+		int[] boardCopy = Arrays.copyOf(this.board, this.board.length);
+		//Swap elements
+		int temp = boardCopy[index1];
+		boardCopy[index1] = boardCopy[index2];
+		boardCopy[index2] = temp;
+		
+		return boardCopy;
+	}
+	
 	public void printState(int option) {
 		
 		// TO DO: print a torus State based on option (flag)
